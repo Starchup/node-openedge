c71ed678924b109ed35969db4b55efb25d600be0
Geoffroy Lesage <gefthefrench@gmail.com>
2020-05-06 19:05:34 +0200

    prelim version

--- openedge.js
+++ openedge.js
@@ -4,11 +4,18 @@
 var jwt = require('jsonwebtoken');
 var rp = require('request-promise');
 var crypto = require('crypto');
+var uuidv4 = require('uuid/v4');
 
-var production = 'https://api.paygateway.com/';
-var sandbox = 'https://api.pit.paygateway.com/';
+var production = 'https://api.paygateway.com';
+var sandbox = 'https://api.pit.paygateway.com';
 var version = '2019-06-27';
-var region = 'US';
+
+var regionToCode = {
+    'US': '840',
+    'CA': '124',
+    'AU': '036',
+    'NZ': '554'
+};
 
 /**
  * Constructor
@@ -16,6 +23,124 @@ var region = 'US';
 var openedge = function (config)
 {
     var self = this;
+
+    self.Request = {
+
+        CreateRequest: function (httpMethod, resource, endpoint, body, query)
+        {
+            self.Util.validateArgument(httpMethod, 'httpMethod');
+            self.Util.validateArgument(resource, 'resource');
+
+            var options = {
+                uri: self.Util.buildUrl(resource, endpoint, query),
+                method: httpMethod,
+                headers:
+                {
+                    'Content-Type': 'application/JSON',
+                    'X-GP-Api-Key': self.apiKey,
+                    'X-GP-Version': version,
+                    'Authorization': 'AuthToken ' + self.authToken, // ? maybe different format: Authorization:AuthToken x.y.z,
+                    'X-GP-Request-ID': 'MER-' + uuidv4()
+                },
+                json: true
+            };
+
+            if (body) options.body = body;
+
+            return rp(options).then(function (res)
+            {
+                var response = JSON.parse(res);
+
+                if (response && response.response) return response.response;
+                if (response && response.Response) return response.Response;
+                if (response && response.results) return response.results;
+
+                throw new Error('No response');
+            });
+        }
+    };
+
+    self.Card = {
+        Create: function (options)
+        {
+            self.Util.validateArgument(options, 'options');
+            self.Util.validateArgument(options.cardNumber, 'options.cardNumber');
+            self.Util.validateArgument(options.expMonth, 'options.expMonth');
+            self.Util.validateArgument(options.expYear, 'options.expYear');
+
+            var body = {
+                card:
+                {
+                    card_number: options.cardNumber,
+                    cardholder_name: options.firstName + ' ' + options.lastName,
+                    expiry_month: options.expMonth,
+                    expiry_year: options.expYear,
+                    card_security_code: options.cvv
+                },
+                customer:
+                {
+                    first_name: options.firstName,
+                    last_name: options.lastName,
+                    billing_address:
+                    {
+                        line1: options.address,
+                        postal_code: options.zipcode,
+                        city: options.city,
+                        country: options.country
+                    }
+                },
+                payment:
+                {
+                    amount: 0,
+                    currency_code: regionToCode[self.region]
+                },
+                transaction:
+                {
+                    country_code: regionToCode[self.region],
+                    processing_indicators:
+                    {
+                        create_token: true
+                    }
+                }
+            };
+
+            return self.Request.CreateRequest('POST', 'transactions', 'auths', body).then(function (res)
+            {
+                if (!res) self.Util.throwInvalidDataError(res);
+
+                var resJSON = xmlP.parse(res);
+                if (!resJSON || !resJSON.txn)
+                {
+                    self.Util.throwInvalidDataError(res);
+                }
+
+                if (resJSON.txn.ssl_result === 1)
+                {
+                    throw new Error('Card could not be tokenized: ' + resJSON.txn.ssl_result_message);
+                }
+
+                return {
+                    foreignId: resJSON.txn.ssl_token
+                };
+            });
+        },
+        Get: function (options)
+        {
+            return;
+        },
+        Sale: function (options)
+        {
+            return;
+        },
+        Void: function (options)
+        {
+            return;
+        },
+        Refund: function (options)
+        {
+            return;
+        }
+    };
 
     self.Util = {
         validateArgument: function (arg, name)
@@ -42,12 +167,12 @@ var openedge = function (config)
 
         generateAuthToken: function ()
         {
-            if (!self.merchant || !self.apiSecret) throw "Auth token could not be genereated: missing data";
+            if (!self.merchant || !self.apiSecret) throw "Auth token could not be generated: missing data";
 
-            var encodedHeaderJSON = encodeURI(self.Util.generateEncodedJSONHeader());
-            var encodedPayLoadJSON = encodeURI(self.Util.generateEncodedPayloadJSON(self.merchant, region));
-            var encodedSignature = encodeURI(self.Util.generateHashSignature(encodedHeaderJSON, encodedPayLoadJSON));
-            return encodedHeaderJSON + "." + encodedPayLoadJSON + "." + encodedSignature;
+            var encodedHeaderJSON = self.Util.generateEncodedJSONHeader();
+            var encodedPayLoadJSON = self.Util.generateEncodedPayloadJSON(self.merchant, self.region);
+            var encodedSignature = self.Util.generateHashSignature(encodedHeaderJSON, encodedPayLoadJSON);
+            return self.Util.urlSafe(encodedHeaderJSON + "." + encodedPayLoadJSON + "." + encodedSignature);
         },
 
 
@@ -75,28 +200,34 @@ var openedge = function (config)
         {
             var buff = new Buffer(JSON.stringify(
             {
-                accountCredential: self.merchant,
-                region: region,
+                account_credential: self.merchant,
+                region: self.region,
                 type: "AuthTokenV2",
                 ts: new Date().getTime()
             }));
             return buff.toString('base64');
+        },
+
+        urlSafe: function (string)
+        {
+            return string.split('+').join('-').split('/').join('_');
         }
     };
 
 
     self.Util.validateArgument(config.merchant, 'merchant');
+    self.Util.validateArgument(config.region, 'region');
     self.Util.validateArgument(config.apiKey, 'apiKey');
     self.Util.validateArgument(config.apiSecret, 'apiSecret');
     self.Util.validateArgument(config.authToken, 'authToken');
     self.Util.validateArgument(config.environment, 'environment');
 
-    self.merchant = config.merchant;
-    self.apiKey = config.apiKey;
-    self.apiSecret = config.apiSecret;
-
     self.baseUrl = config.environment === 'Production' ? production : sandbox;
 
+    self.merchant = config.merchant;
+    self.region = config.region;
+    self.apiKey = config.apiKey;
+    self.apiSecret = config.apiSecret;
     self.authToken = self.Util.generateAuthToken();
 
     return self;
--- test.js
+++ test.js
@@ -10,11 +10,95 @@ var Openedge = new openedge(
     apiKey: '',
     apiSecret: '',
     authToken: '',
-    environment: '',
+    environment: 'sandbox',
+    region: 'US'
 });
 
-var customerForeignId, cardForeignId, transactionForeignId;
+var cardForeignId, transactionForeignId;
 
-describe('Customer Methods', function () {
+describe('Card Methods', function ()
+{
+    it('should create a credit card on openedge', function (done)
+    {
+        Openedge.Card.Create(
+        {
+            cardNumber: '5454545454545454',
+            expMonth: '12',
+            expYear: '20',
+            cvv: '400',
+            firstName: 'Q',
+            lastName: 'Smith',
+            street: '1 Secret Road',
+            city: 'London',
+            country: 'UK',
+            zipcode: 'W30 UE'
+        }).then(function (cardData)
+        {
+            expect(cardData).to.exist; // jshint ignore:line
+            expect(cardData.foreignId).to.be.above(0);
 
+            cardForeignId = cardData.foreignId;
+
+            done();
+        }).catch(done);
+    });
+
+    // it('should get a credit card from openedge', function (done)
+    // {
+    //     Openedge.Customer.GetCards(
+    //     {
+    //         foreignKey: customerForeignId
+
+    //     }).then(function (res)
+    //     {
+    //         expect(res.length).to.be.above(0);
+    //         done();
+    //     }).catch(done);
+    // });
+
+    // it('should bill a credit card on openedge', function (done)
+    // {
+    //     Openedge.Card.Sale(
+    //     {
+    //         foreignKey: cardForeignId,
+    //         amount: 1
+    //     }).then(function (saleData)
+    //     {
+    //         expect(saleData).to.exist;
+    //         expect(saleData.foreignId).to.be.above(0);
+    //
+    //         transactionForeignId = saleData.foreignId;
+    //
+    //         done();
+    //     }).catch(done);
+    // });
+
+    // it('should refund a credit card on openedge', function (done)
+    // {
+    //     Openedge.Card.Refund(
+    //     {
+    //         foreignKey: cardForeignId,
+    //         transactionForeignKey: transactionForeignId,
+    //         amount: 0.5
+    //     }).then(function (refundData)
+    //     {
+    //         expect(refundData).to.exist;
+    //         expect(refundData.foreignId).to.be.above(0);
+    //         done();
+    //     }).catch(done);
+    // });
+
+    // it('should void a credit card on openedge', function (done)
+    // {
+    //     Openedge.Card.Void(
+    //     {
+    //         foreignKey: cardForeignId,
+    //         transactionForeignKey: transactionForeignId
+    //     }).then(function (voidData)
+    //     {
+    //         expect(voidData).to.exist;
+    //         expect(voidData.foreignId).to.be.above(0);
+    //         done();
+    //     }).catch(done);
+    // });
 });
